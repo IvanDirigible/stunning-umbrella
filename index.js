9c34e62e078f033e7751b6dd1b25b0059f3e394a
IvanDirigible <sickeruncible@gmail.com>
2024-02-09 22:16:13 -0500

    All color tests working. Validation good. Deleted extra files and stray comments.

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { Circle, Triangle, Square } = require('./lib/shapes')
-const colorKeywords = require('./lib/colorKeywords')
-
-// const inputTypeValidator = (input) => {
-//     if (input !== colorKeywords.includes(input.toLowerCase()) || input !== /0x[\da-f]/i.test(input)) {
-//         return console.log("\nWe couldn't find this color. Please try again.")
-//     } else
-//     return true
-// };
+const colorValidate = require('./lib/colorKeywords')
 
 const questions = [
     {
         type: 'input',
         name: 'text',
         message: 'Please enter up to three characters you want in your logo.',
-        validate: (answer) => {
-            if (answer.length > 3) {
+        validate: (res) => {
+            if (res.length > 3) {
                 return console.log("\nYou cannot add more than three characters to the logo. Please try again.")
             }
             return true
@@ -26,34 +19,13 @@ const questions = [
         type: 'input',
         name: 'textFill',
         message: 'Please enter your desired text color with either a color keyword or the hexidecimal number.',
-        // validate: (answer) => {
-        //     if (answer !== colorKeywords.includes(answer.toLowerCase()) || answer !== /0x[\da-f]/i.test(answer)) {
-        //         console.log(`Answer: ${answer}\n Answer.thing: ${answer.inputTextFill}`);
-        //         return console.log("\nWe couldn't find this color. Please try again.")
-        //     }
-        //     return true
-        // },
+        validate: (res) => {
+            if (colorValidate(res)) {
+                return true
+                }
+                return console.log("\nWe couldn't find that color. Please try again.")
+        },
     },
-    // {
-    //     type: 'input',
-    //     name: 'inputTextKeyword',
-    //     message: "Please enter your desired color and we'll see if we can find it.",
-    //     when: (answers) => {
-    //         if (answers.inputTextFill === 'Color Keyword') {
-    //             return true
-    //         }
-    //     },
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'inputTextHex',
-    //     message: "Please enter the hexidecimal number of your desired color.",
-    //     when: (answers) => {
-    //         if (answers.inputTextFill === 'Hexidecimal Number') {
-    //             return true
-    //         }
-    //     },
-    // },
     {
         type: 'list',
         name: 'shape',
@@ -64,27 +36,13 @@ const questions = [
         type: 'input',
         name: 'shapeFill',
         message: 'Please enter your desired shape color with either a color keyword or the hexidecimal number.',
+        validate: (res) => {
+            if (colorValidate(res)) {
+                return true
+                }
+                return console.log("\nWe couldn't find that color. Please try again.")
+        },
     },
-    // {
-    //     type: 'input',
-    //     name: 'inputShapeKeyword',
-    //     message: "Please enter your desired color and we'll see if we can find it.",
-    //     when: (answers) => {
-    //         if (answers.inputShapeFill === 'Color Keyword') {
-    //             return true
-    //         }
-    //     },
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'inputShapeHex',
-    //     message: "Please enter the hexidecimal number of your desired color.",
-    //     when: (answers) => {
-    //         if (answers.inputShapeFill === 'Hexidecimal Number') {
-    //             return true
-    //         }
-    //     },
-    // },
 ];
 
 function generateLogo(data) {
